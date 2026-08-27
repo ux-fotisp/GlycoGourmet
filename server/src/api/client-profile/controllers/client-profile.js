848b93ac56e916f470f359443a345643073c8a58
FotisP <fotispastrakis@gmail.com>
2026-08-27 14:59:39 +0300

    fix: revert to manual service call and explicit nested syntax to avoid validation errors

--- server/src/api/client-profile/controllers/client-profile.js
+++ server/src/api/client-profile/controllers/client-profile.js
@@ -1,33 +1,47 @@
 "use strict";
-
 const { createCoreController } = require("@strapi/strapi").factories;
-
 module.exports = createCoreController("api::client-profile.client-profile", ({ strapi }) => ({
   async find(ctx) {
-    const user = ctx.state.user;
-    if (user && user.roleType === 'dietitian') {
-      ctx.query = {
-        ...ctx.query,
-        filters: {
-          ...(ctx.query.filters || {}),
-          dietitian: user.id
-        }
-      };
+    try {
+      await this.validateQuery(ctx);
+      const sanitizedQuery = await this.sanitizeQuery(ctx);
+      const user = ctx.state.user;
+      if (user && user.roleType === 'dietitian') {
+        sanitizedQuery.filters = {
+          ...(sanitizedQuery.filters || {}),
+          dietitian: { id: { $eq: user.id } }
+        };
+      }
+      const { results, pagination } = await strapi.service("api::client-profile.client-profile").find(sanitizedQuery);
+      const sanitizedResults = await this.sanitizeOutput(results, ctx);
+      return this.transformResponse(sanitizedResults, { pagination });
+    } catch (err) {
+      console.error('FIND ERROR:', err);
+      throw err;
     }
-    return super.find(ctx);
   },
-  
   async findOne(ctx) {
-    const user = ctx.state.user;
-    if (user && user.roleType === 'dietitian') {
-      ctx.query = {
-        ...ctx.query,
-        filters: {
-          ...(ctx.query.filters || {}),
-          dietitian: user.id
-        }
-      };
+    try {
+      await this.validateQuery(ctx);
+      const sanitizedQuery = await this.sanitizeQuery(ctx);
+      const user = ctx.state.user;
+      if (user && user.roleType === 'dietitian') {
+        sanitizedQuery.filters = {
+          ...(sanitizedQuery.filters || {}),
+          dietitian: { id: { $eq: user.id } }
+        };
+      }
+      const entity = await strapi.service("api::client-profile.client-profile").findOne(ctx.params.id, sanitizedQuery);
+      if (!entity) return ctx.notFound();
+      if (user && user.roleType === 'dietitian') {
+        const ownerId = entity.dietitian?.id || entity.dietitian;
+        if (Number(ownerId) !== Number(user.id)) return ctx.notFound();
+      }
+      const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+      return this.transformResponse(sanitizedEntity);
+    } catch (err) {
+      console.error('FINDONE ERROR:', err);
+      throw err;
     }
-    return super.findOne(ctx);
   }
 }));
